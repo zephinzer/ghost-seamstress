ba0727c42815457f997e8bd4fe95cacc518a0606
Joseph Matthias Goh <zephinzer@users.noreply.github.com>
2016-12-09 09:05:13 +0800

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const webpackConfigProvider = require('../../../../providers/webpack.config');
-const environment = process.env.NODE_ENV || 'development';
-console.log('Provisioning in', environment, 'environment');
-module.exports = webpackConfigProvider[environment](path.join(__dirname, '/src/index.js'), __dirname);
